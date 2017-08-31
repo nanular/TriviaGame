3d566a4608a05aa81ed5123a7f2d47dc077830a0
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-08-31 03:29:36 -0500

    basic beginnings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,91 @@
 $(document).ready(function() {
 
+var secondsRemaining = 30;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var unansweredQuestions = 0;
+
+var trivia = [
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  },
+  {
+    question: "",
+    answerValue: "",
+    answeredCorrectly: false
+  }
+];  
+
+
+$("#startButton").click(function() {
+  $("#startQuiz").toggle();
+  $(".allQuestions").toggle();
+  $("#questionTimer").html("0:" + secondsRemaining + " Remaining");
+  startQuestionTimer();
+})  
+
+function startQuestionTimer() {
+  var intervalID = setInterval(timerCountdown, 1000);
+  function timerCountdown() {
+    secondsRemaining--;
+    if(secondsRemaining === 0) {
+      clearInterval(intervalID);
+      $("#questionTimer").html("0:0" + secondsRemaining + " Remaining");
+      $("#timerMessage").html("Time Is Up!");
+    }
+    
+    else if(secondsRemaining < 10) {
+      $("#questionTimer").html("0:0" + secondsRemaining + " Remaining");
+    }
+
+    else {
+      $("#questionTimer").html("0:" + secondsRemaining + " Remaining");
+    }
+  }
+} 
 
 
 
-
-
-
-
-
-
-
-
-})	
+})  
