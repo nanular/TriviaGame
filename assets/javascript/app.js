b88938324ad2e2cb4e65475bda5ee0d5f38abb59
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-08-31 20:34:17 -0500

    mostly functional

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 var secondsRemaining = 30;
 var intervalID;
+var intervalIDShort;
 var answerButtonLock = false;
 
 var numberCorrect = 0;
@@ -21,9 +22,24 @@ var trivia = [
 	{
 		question: "Final Fantasy II was released for the Super Nintendo in North America in 1991. What title was the game released under in Japan?",
 		answers: ["Final Fantasy IV", "Final Fantasy II: The New Final Chapter", "Fighting Fantasy", "Final Fantasy: Dawn Of The Spirits"]
+	},
+	{
+		question: "Who was selected first overall in the 1997 NBA draft?",
+		answers: ["Tim Duncan", "Elton Brand", "Allen Iverson", "Shaquille O'Neal"]
+	},
+	{
+		question: "Which of these following bands did NOT hit the height of their popularity in the 1990's?",
+		answers: ["Arctic Monkeys", "Crash Test Dummies", "Sponge", "Veruca Salt"]
+	},
+	{
+		question: "What song did Microsoft pay millions for to use in the promotion of their new Windows 95 operating system?",
+		answers: ["'Start Me Up' by The Rolling Stones", "'Don't Let's Start' by They Might Be Giants", "'We Didn't Start The Fire' by Billy Joel", "'Get The Party Started' by Pink"]
+	},
+	{
+		question: "What lyrics come next?  So I, I turned the radio on, I turned the radio up and this woman was singin' my song..",
+		answers: ["The lover's in love and the other's run away", "And you say, I only hear what I want to", "Now I know that I did something wrong", "Yeah, I missed you."]
 	}
 ]
-
 
 
 
@@ -73,37 +89,53 @@ function randomizeAnswers() {
 function questionTimer() {
 	secondsRemaining = 30;
 	$("#timerDisplay").html("0:" + secondsRemaining);
-	intervalID = setInterval(timerCountdown, 1000);
+	intervalID = window.setInterval(timerCountdown, 1000);
 	function timerCountdown() {
 		secondsRemaining--;
 		if(secondsRemaining === 0) {
-			clearInterval(intervalID);
 			$("#timerDisplay").html("0:0" + secondsRemaining);
-      	$("#triviaMessage").html("Time Is Up!");
-      	numberUnanswered++;
-      	answerButtonLock = true;
-		} else if (secondsRemaining < 28) {
-			$("#triviaMessage").html("")
+      		$("#triviaMessage").html("Time Is Up!");
+      		numberUnanswered++;
+      		currentQuestionIndex++;
+      		answerButtonLock = true;
+      		clearInterval(intervalID);
+      		loadNextQuestion();
+      		questionTimer();
+      		return;
 		} else if(secondsRemaining < 10) {
 			$("#timerDisplay").html("0:0" + secondsRemaining);
 		} else {
 			$("#timerDisplay").html("0:" + secondsRemaining);
 		}
+
+		if(secondsRemaining === 10) {
+			var tenSeconds = new Audio("assets/sounds/tenSeconds.mp3");
+			tenSeconds.volume = 0.50;
+			tenSeconds.play();
+		} else if(secondsRemaining === 28) {
+			$("#triviaMessage").html("  ");
+		}
 	}
 }
 
 
-$(".btn").click(function() {
-	if($(this).text() === trivia[currentQuestionIndex].answers[0] && !answerButtonLock) {
-		answerButtonLock = true;
-		$("#triviaMessage").html("Congratulations!<br>That is correct!");
+$(".btn-primary").click(function() {
+	answerButtonLock = true;
+	if($(this).text() === trivia[currentQuestionIndex].answers[0]) {
+		$("#triviaMessage").html("That is correct!");
 		numberCorrect++;
 		currentQuestionIndex++;
 		clearInterval(intervalID);
 		loadNextQuestion();
 		questionTimer();
 	} else {
-
+		numberWrong++;
+		$("#triviaMessage").html("Incorrect!");
+		answerButtonLock = true;
+		currentQuestionIndex++;
+		clearInterval(intervalID);
+		loadNextQuestion();
+		questionTimer();
 	}
 
 }) 
