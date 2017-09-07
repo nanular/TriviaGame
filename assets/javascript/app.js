4711ae50b758828994ec5eef44931135cff3d3e6
Neil Zimmerman <Neil@MacBook-Pro-13-Mid-2012.local>
2017-09-07 09:36:35 -0500

    merging to latest version

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 
+
 var secondsRemaining = 30;
 var intervalID;
-var intervalIDShort;
 var answerButtonLock = false;
 
 var numberCorrect = 0;
@@ -10,9 +10,13 @@ var numberWrong = 0;
 var numberUnanswered = 0;
 var currentQuestionIndex = 0;
 
+
+//Extra questoins may be added without making any additional code changes.
+//The correct answer should be placed in the zero index of the "answers" array.
+//The program will randomly organized all 4 given answers to choose from.
 var trivia = [
 	{
-		question: "Which of the following bands released the hit song 'Disarm' in 1994?",
+		question: "Which band released the hit song 'Disarm' in 1994?",
 		answers: ["The Smashing Pumpkins", "Soundgarden", "The Meat Puppets", "Pearl Jam"]
 	},
 	{
@@ -36,25 +40,36 @@ var trivia = [
 		answers: ["'Start Me Up' by The Rolling Stones", "'Don't Let's Start' by They Might Be Giants", "'We Didn't Start The Fire' by Billy Joel", "'Get The Party Started' by Pink"]
 	},
 	{
-		question: "What lyrics come next?  So I, I turned the radio on, I turned the radio up and this woman was singin' my song..",
+		question: "So I, I turned the radio on, I turned the radio up and this woman was singin' my song...",
 		answers: ["The lover's in love and the other's run away", "And you say, I only hear what I want to", "Now I know that I did something wrong", "Yeah, I missed you."]
+	},
+	{
+		question: "Which of the following technologies were NOT introduced to the world in the 1990's?",
+		answers: ["Lotus Notes", "Iomega Zip Drive", "Microsoft Windows 3.0", "Adobe Photoshop"]
+	},
+	{
+		question: "In 1996, which republican candidate did Bill Clinton defeat in the presedential election?",
+		answers: ["Bob Dole", "Michael Dukakis", "Al Gore", "George H. W. Bush"]
+	},
+	{
+		question: "Which of the following sitcoms aired both their first and last episodes in the 1990's?",
+		answers: ["Home Improvement", "Friends", "The Simpsons", "The X-Files"]
 	}
-]
+];
 
 
 
-$("#startButton").click(function() {
-	$("#startQuiz").toggle();
-	$(".allQuestions").toggle();
-	$(".allAnswers").toggle();
+$("#startButton").click(function()
+{
+	$("#gameStart, .quizDiv, .allAnswers, .messageSection").toggle();
 	loadNextQuestion();
 	questionTimer();
-})
+});
 
 
 
-
-function loadNextQuestion() {
+function loadNextQuestion()
+{
 	answerButtonLock = false;
 	$("#questionHeader").html("Question " + (currentQuestionIndex + 1))
 	$("#questionParagraph").html(trivia[currentQuestionIndex].question);
@@ -63,18 +78,20 @@ function loadNextQuestion() {
 
 
 
-function randomizeAnswers() {
+function randomizeAnswers()
+{
 	var random = Math.floor(Math.random() * 4);
 	var buttonOne = random;
 	$("#buttonOne").text(trivia[currentQuestionIndex].answers[buttonOne]);
-
-	while(random === buttonOne) {
+	while(random === buttonOne)
+	{
 		random = Math.floor(Math.random() * 4);
 	}
 	var buttonTwo = random;
 	$("#buttonTwo").text(trivia[currentQuestionIndex].answers[buttonTwo]);
 
-	while(random === buttonOne || random === buttonTwo) {
+	while(random === buttonOne || random === buttonTwo)
+	{
 		random = Math.floor(Math.random() * 4);
 	}
 	var buttonThree = random;
@@ -86,63 +103,135 @@ function randomizeAnswers() {
 
 
 
-function questionTimer() {
+function questionTimer()
+{
 	secondsRemaining = 30;
 	$("#timerDisplay").html("0:" + secondsRemaining);
 	intervalID = window.setInterval(timerCountdown, 1000);
-	function timerCountdown() {
+	function timerCountdown()
+	{
 		secondsRemaining--;
-		if(secondsRemaining === 0) {
-			$("#timerDisplay").html("0:0" + secondsRemaining);
+		if(secondsRemaining === 0)
+		{
+			$("#timerDisplay").html("0:00");
       		$("#triviaMessage").html("Time Is Up!");
       		numberUnanswered++;
-      		currentQuestionIndex++;
-      		answerButtonLock = true;
-      		clearInterval(intervalID);
-      		loadNextQuestion();
-      		questionTimer();
-      		return;
-		} else if(secondsRemaining < 10) {
+     		 currentQuestionIndex++;
+     		 answerButtonLock = true;
+     		 clearInterval(intervalID);
+     		 loadNextQuestion();
+     		 questionTimer();
+     		 return;
+		} else if(secondsRemaining < 10)
+		{
 			$("#timerDisplay").html("0:0" + secondsRemaining);
-		} else {
+		} else
+		{
 			$("#timerDisplay").html("0:" + secondsRemaining);
 		}
 
-		if(secondsRemaining === 10) {
+		if(secondsRemaining === 10)
+		{
 			var tenSeconds = new Audio("assets/sounds/tenSeconds.mp3");
 			tenSeconds.volume = 0.50;
 			tenSeconds.play();
-		} else if(secondsRemaining === 28) {
+		} else if(secondsRemaining === 27)
+		{
 			$("#triviaMessage").html("  ");
 		}
 	}
 }
 
 
-$(".btn-primary").click(function() {
+
+$(".btn-primary").click(function()
+{
 	answerButtonLock = true;
-	if($(this).text() === trivia[currentQuestionIndex].answers[0]) {
-		$("#triviaMessage").html("That is correct!");
+	if($(this).text() === trivia[currentQuestionIndex].answers[0])
+	{
+		$("#triviaMessage").html("Correct!<br>");
 		numberCorrect++;
-		currentQuestionIndex++;
-		clearInterval(intervalID);
-		loadNextQuestion();
-		questionTimer();
-	} else {
+	} else
+	{
 		numberWrong++;
-		$("#triviaMessage").html("Incorrect!");
-		answerButtonLock = true;
-		currentQuestionIndex++;
+		$("#triviaMessage").html("Incorrect!<br><br>Correct Answer:<br><strong>"
+			+ trivia[currentQuestionIndex].answers[0] + "</strong>");
+	}
+	currentQuestionIndex++;
+
+	if(currentQuestionIndex === trivia.length)
+	{
+		var standby = new Audio("assets/sounds/standby.mp3");
+		standby.volume = 0.50;
+		standby.play();
+
+		clearInterval(intervalID);
+		secondsRemaining = 5;
+		intervalID = window.setInterval(lastQuestionCountdown, 1000);
+		function lastQuestionCountdown()
+		{
+			secondsRemaining--;
+			if(secondsRemaining === 0)
+			{
+				$("#triviaMessage").html("  ");
+				$(".allAnswers, .quizDiv, .messageSection").toggle();
+				displayResults(numberWrong, numberCorrect, numberUnanswered);
+			}
+		}
+	} else
+	{
 		clearInterval(intervalID);
 		loadNextQuestion();
 		questionTimer();
 	}
-
-}) 
-
+});
 
 
 
+function displayResults()
+{
+	var percentageScore = (numberCorrect / trivia.length);
+	percentageScore = (percentageScore * 100).toFixed(2);
+	
+	$("#quizResults").toggle();
+	$("#yourScore").append(percentageScore + "&#37;");
+	$("#correctlyAnswered").append(numberCorrect);
+	$("#incorrectlyAnswered").append(numberWrong);
+	$("#unansweredQuestions").append(numberUnanswered);
+
+	if(percentageScore < 50)
+	{
+		var nextTime = new Audio("assets/sounds/nextTime.mp3");
+		nextTime.volume = 0.50;
+		nextTime.play();
+	} else if(percentageScore >= 70)
+	{
+		var congratulations = new Audio("assets/sounds/congratulations.mp3");
+		congratulations.volume = 0.50;
+		congratulations.play();
+	}
+}
+
+
+
+$("#resetButton").click(function()
+{
+	clearInterval(intervalID);
+	numberCorrect = 0;
+	numberWrong = 0;
+	numberUnanswered = 0;
+	currentQuestionIndex = 0;
+	secondsRemaining = 30;
+	answerButtonLock = false;
+
+	$("#quizResults").toggle();
+	$("#gameStart").toggle();
+
+	$("#yourScore").text("Your Score: ");
+	$("#correctlyAnswered").text("Questions Answered Correctly: ");
+	$("#incorrectlyAnswered").text("Questions Answered Incorrectly: ");
+	$("#unansweredQuestions").text("Unanswered Questions: ");
+});
 
 
 
